2c3ac9b2c836e54b0921884d97f16e205cb780cb
Rokas <rookas.rudzenskas@gmail.com>
2022-10-03 18:58:24 +0300

    Rokas

--- components/ContactsList.js
+++ components/ContactsList.js
@@ -3,7 +3,7 @@ import {Text, View, StyleSheet, FlatList} from 'react-native';
 const ContactsList = () => {
     const contacts = ['Rokas', 'Tom', 'James', 'Steve', 'Jen', 'Kukulis'];
     return (
-        <View>
+        <View className="">
             <FlatList data={contacts} renderItem={({item}) => {
                 return <Text className="text-black">{item}</Text>
             }} />
