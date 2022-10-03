7996bbb722164ccb31b7b6c8b52ff92633ca043a
Rokas <rookas.rudzenskas@gmail.com>
2022-10-03 20:09:34 +0300

    Rokas

--- screens/ContactsScreen/index.js
+++ screens/ContactsScreen/index.js
@@ -1,4 +1,4 @@
-import {Text, View, StyleSheet, FlatList, TextInput} from 'react-native';
+import {Text, View, StyleSheet, FlatList, TextInput, TouchableOpacity} from 'react-native';
 import ContactsData from '../../data/contacts.json';
 import { Ionicons } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
@@ -11,9 +11,13 @@ const ContactsScreen = () => {
     return (
         <View className="">
             <View className="flex-row items-center justify-between mb-4">
-                <Text className="text-[#2886C4] text-lg font-[400]">Groups</Text>
+                <TouchableOpacity>
+                    <Text className="text-[#2886C4] text-lg font-[400]">Groups</Text>
+                </TouchableOpacity>
                 <Text className="text-black text-lg font-bold -ml-6">Contacts</Text>
-                <AntDesign name="plus" size={24} color="#2886C4" />
+                <TouchableOpacity>
+                    <AntDesign name="plus" size={24} color="#2886C4" />
+                </TouchableOpacity>
             </View>
             <View className="flex-row items-center bg-gray-200 py-1 rounded-md space-x-2">
                 <Ionicons name="search" size={24} color="gray" className="ml-2" style={{marginLeft: 10}}/>
