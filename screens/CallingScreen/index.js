c6e3b3f0b5c853f5ebfb453c7c3049c2872ef461
Rokas <rookas.rudzenskas@gmail.com>
2022-10-04 09:18:27 +0300

    Rokas

--- screens/CallingScreen/index.js
+++ screens/CallingScreen/index.js
@@ -4,10 +4,14 @@ import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const CallingScreen = () => {
     return (
-        <View>
-            <Text>
-                byrookas 🚀
-            </Text>
+        <View className="mt-10 h-screen items-center bg-blue-200">
+            <View className="mt-32">
+                <Text className="text-2xl text-white font-bold text-center">Rokas //:</Text>
+                <Text className="text-[19px] text-gray-400 font-[500] mt-1 animate-pulse">ringing +31 0343 3423 349</Text>
+            </View>
+            <View>
+
+            </View>
         </View>
     );
 };
