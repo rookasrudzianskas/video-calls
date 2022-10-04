40fe5552d7775f647b9bf34c2c8e233cdd4ac3d4
Rokas <rookas.rudzenskas@gmail.com>
2022-10-04 12:09:05 +0300

    Rokas

--- screens/CallingScreen/index.js
+++ screens/CallingScreen/index.js
@@ -4,11 +4,13 @@ import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import {Feather, Ionicons, SimpleLineIcons} from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
 import CallActionBox from "../../components/CallActionBox";
-import {useNavigation} from "@react-navigation/native";
+import {useNavigation, useRoute} from "@react-navigation/native";
 
 
 const CallingScreen = () => {
     const navigation = useNavigation();
+    const route = useRoute();
+    const user = route?.params?.user;
 
     return (
         <View className="pt-16 h-screen items-center bg-blue-200">
@@ -16,7 +18,7 @@ const CallingScreen = () => {
                 <Ionicons name="ios-chevron-back" size={30} color="white" />
             </TouchableOpacity>
             <View className="mt-32 flex-1">
-                <Text className="text-2xl text-white font-bold text-center">Rokas //:</Text>
+                <Text className="text-2xl text-white font-bold text-center">{user?.user_display_name || 'Loading...'} //:</Text>
                 <Text className="text-[19px] text-gray-400 font-[500] mt-1 animate-pulse">ringing +31 0343 3423 349</Text>
             </View>
             <CallActionBox />
