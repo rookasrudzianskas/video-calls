60b63f0290223cbf858aeb7d07e6b4ff5ef5a2bb
Rokas <rookas.rudzenskas@gmail.com>
2022-10-04 09:10:23 +0300

    Rokas

---
+++ screens/CallingScreen/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const CallingScreen = () => {
+    return (
+        <View>
+            <Text>
+                byrookas 🚀
+            </Text>
+        </View>
+    );
+};
+
+export default CallingScreen;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import ContactsList from "../components/ContactsList";
 import ContactsScreen from "./ContactsScreen";
+import CallingScreen from "./CallingScreen";
 
 const HomeScreen = () => {
     return (
         <View>
-            <ContactsScreen />
+            {/*<ContactsScreen />*/}
+            <CallingScreen />
         </View>
     );
 };
