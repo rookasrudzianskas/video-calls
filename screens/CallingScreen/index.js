2b63fb4c2a7dd521448250dd692a1cce6a9d66c2
Rokas <rookas.rudzenskas@gmail.com>
2022-10-04 09:41:13 +0300

    Rokas

--- screens/CallingScreen/index.js
+++ screens/CallingScreen/index.js
@@ -5,7 +5,7 @@ import {Feather, Ionicons, SimpleLineIcons} from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
 const CallingScreen = () => {
     return (
-        <View className="mt-10 h-screen items-center bg-blue-200">
+        <View className="pt-16 h-screen items-center bg-blue-200">
             <View className="mt-32 flex-1">
                 <Text className="text-2xl text-white font-bold text-center">Rokas //:</Text>
                 <Text className="text-[19px] text-gray-400 font-[500] mt-1 animate-pulse">ringing +31 0343 3423 349</Text>
