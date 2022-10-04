f14caca658475942294608f937c4f9c3766631bf
Rokas <rookas.rudzenskas@gmail.com>
2022-10-04 10:01:29 +0300

    Rokas

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,12 +3,14 @@ import {Text, View, StyleSheet} from 'react-native';
 import ContactsList from "../components/ContactsList";
 import ContactsScreen from "./ContactsScreen";
 import CallingScreen from "./CallingScreen";
+import IncomingCallScreen from "./IcomingCallScreen";
 
 const HomeScreen = () => {
     return (
         <View>
             {/*<ContactsScreen />*/}
-            <CallingScreen />
+            {/*<CallingScreen />*/}
+            <IncomingCallScreen />
         </View>
     );
 };
---
+++ screens/IcomingCallScreen/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {Text, View, StyleSheet, TouchableOpacity, ImageBackground} from 'react-native';
+import {
+    AntDesign,
+    Feather,
+    FontAwesome5,
+    Ionicons,
+    MaterialCommunityIcons,
+    MaterialIcons,
+    SimpleLineIcons
+} from "@expo/vector-icons";
+import BackgroundIos from '../../assets/images/ios_bg.png';
+
+const IncomingCallScreen = () => {
+    return (
+        <ImageBackground source={BackgroundIos} resizeMode={"cover"} className="w-full h-full">
+            <View className="mt-32 flex-1 items-center">
+                <Text className="text-2xl text-white font-bold text-center">Rokas //:</Text>
+                <View className="flex-row items-center mt-1 space-x-1">
+                    <FontAwesome5 name="whatsapp-square" size={17} color="green" />
+                    <Text className="text-[19px] text-gray-300 font-[500] animate-pulse">BoringApp Video...</Text>
+                </View>
+            </View>
+            <View className="flex-col h-[300px] w-full rounded-t-xl px-10">
+                <View className="flex-row justify-between w-full">
+                    <TouchableOpacity activeOpacity={0.7} className="flex-col items-center justify-center ">
+                        <MaterialCommunityIcons name="timer-outline" size={22} color="white" />
+                        <Text className="text-white mt-2">Remind Me</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity activeOpacity={0.7} className="flex-col items-center justify-center ">
+                        <FontAwesome5 name="sms" size={22} color="white" />
+                        <Text className="text-white mt-2">Message</Text>
+                    </TouchableOpacity>
+                </View>
+                <View className="flex-row justify-between w-full mt-20">
+                    <TouchableOpacity activeOpacity={0.7} className="flex-col w-16 h-16 rounded-full bg-red-500 items-center justify-center ">
+                        <AntDesign name="close" size={35} color="white" />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity activeOpacity={0.7} className="flex-col w-16 h-16 rounded-full bg-blue-500 items-center justify-center ">
+                        <Ionicons name="checkmark" size={35} color="white" />
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </ImageBackground>
+    );
+};
+
+export default IncomingCallScreen;
