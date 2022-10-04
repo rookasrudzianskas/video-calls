7daef9b1e68951a00aa97bd112cc53f1ab236f3d
Rokas <rookas.rudzenskas@gmail.com>
2022-10-04 13:47:01 +0300

    Rokas

--- screens/LoginScreen/index.js
+++ screens/LoginScreen/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, View, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
 
 const LoginScreen = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <View className="bg-gray-100 h-screen w-full pt-10 items-start justify-center px-5 space-y-3">
             <Text className="uppercase text-3xl font-semibold text-gray-700">Login</Text>
             <View className="bg-white w-full py-2 px-2 rounded-lg border border-blue-500/30">
-                <TextInput placeholder={'Username'} />
+                <TextInput autoCapitalize={false} value={username} onChangeText={(e) => setUsername(e)} placeholder={'Username'} />
             </View>
             <View className="bg-white w-full py-2 px-2 rounded-lg border border-blue-500/30">
-                <TextInput placeholder={'Password'} />
+                <TextInput value={password} onChangeText={(e) => setPassword(e)} secureTextEntry={true} placeholder={'Password'} />
             </View>
 
             <TouchableOpacity className="bg-blue-400 w-full items-center justify-center py-2 rounded-lg" activeOpacity={0.7}>
